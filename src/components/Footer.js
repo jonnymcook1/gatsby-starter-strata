3c76dee24c6dd24288c9360b47ec4b50e78fdfc6
johncook <jonnymcook1@gmail.com>
2019-07-21 15:26:44 -0500

    portfolio commit 1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+// import {Link} from 'react-router-dom'
+
 
 class Footer extends React.Component {
     render() {
@@ -6,13 +8,11 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="#" className="icon fa-linkedin"><span className="label">Linkedin</span></a></li>
+                       <li><a href="https://github.com/jonnymcook1" className="icon fa-github"><span className="label" >Github</span></a></li>
                     </ul>
                     <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                        {/* <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li> */}
                     </ul>
                 </div>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -45,12 +45,11 @@ class HomeIndex extends React.Component {
 
                     <section id="one">
                         <header className="major">
-                            <h2>Ipsum lorem dolor aliquam ante commodo<br />
-                            magna sed accumsan arcu neque.</h2>
+                            <h2>About John</h2>
                         </header>
-                        <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
+                        <p>I am a Web Developer living in Dallas, Tx.  Born in Tulsa, Ok and US Air Force veteran. I started my Developer journey in August, 2019. I am proficient in working with React.js, Node.js, and PostgreSQL to build web applications with high functionality and clean designs. I love working on projects that bring value to people. lets build one!</p>
                         <ul className="actions">
-                            <li><a href="#" className="button">Learn More</a></li>
+                            {/* <li><a href="#" className="button">Learn More</a></li> */}
                         </ul>
                     </section>
 
@@ -70,8 +69,8 @@ class HomeIndex extends React.Component {
                     </section>
 
                     <section id="three">
-                        <h2>Get In Touch</h2>
-                        <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
+                        <h2>Contact</h2>
+                        {/* <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p> */}
                         <div className="row">
                             <div className="8u 12u$(small)">
                                 <form method="post" action="#">
@@ -95,11 +94,11 @@ class HomeIndex extends React.Component {
                                     </li>
                                     <li>
                                         <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                                        000-000-0000
+                                        405-822-5743
                                     </li>
                                     <li>
                                         <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
-                                        <a href="#">hello@untitled.tld</a>
+                                        <a href="#">jonnymcook1@gmail.com</a>
                                     </li>
                                 </ul>
                             </div>
